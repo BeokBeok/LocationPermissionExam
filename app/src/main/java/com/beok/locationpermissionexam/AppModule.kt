ff13ec1c1583156cd0e3b8bbe0ad8afbce379ae8
강현석 <kekemusa37@gmail.com>
2021-04-17 21:12:06 +0900

    [추가] AppModule

---
+++ app/src/main/java/com/beok/locationpermissionexam/AppModule.kt
@@ -0,0 +1,20 @@
+package com.beok.locationpermissionexam
+
+import android.content.Context
+import android.location.LocationManager
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class AppModule {
+
+    @Provides
+    @Singleton
+    fun providesLocationManager(@ApplicationContext context: Context) =
+        context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+}
