6a74d6b1cee481ff3e88d8130e9fc8c2879866f3
강현석 <kekemusa37@gmail.com>
2021-04-17 21:44:05 +0900

    [구현] 위도 경도 가져오는 함수

---
+++ app/src/main/java/com/beok/locationpermissionexam/Coordinate.kt
@@ -0,0 +1,3 @@
+package com.beok.locationpermissionexam
+
+data class Coordinate(val longitude: Double = 0.0, val latitude: Double = 0.0)
--- app/src/main/java/com/beok/locationpermissionexam/LocationUtil.kt
+++ app/src/main/java/com/beok/locationpermissionexam/LocationUtil.kt
@@ -1,9 +1,17 @@
 package com.beok.locationpermissionexam
 
+import android.annotation.SuppressLint
 import android.location.LocationManager
 import javax.inject.Inject
 
 class LocationUtil @Inject constructor(private val locationManager: LocationManager) {
 
     fun checkGPS() = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+
+    @SuppressLint("MissingPermission")
+    fun coordinate(): Coordinate =
+        (locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+            ?: locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER))?.let {
+            Coordinate(longitude = it.longitude, latitude = it.latitude)
+        } ?: Coordinate()
 }
