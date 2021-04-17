b08d7be4cc38d7863d2a6ef6e543cbe97bb3586d
강현석 <kekemusa37@gmail.com>
2021-04-17 21:24:44 +0900

    [구현] 위치 권한 요청

--- app/src/main/java/com/beok/locationpermissionexam/MainActivity.kt
+++ app/src/main/java/com/beok/locationpermissionexam/MainActivity.kt
@@ -1,11 +1,54 @@
 package com.beok.locationpermissionexam
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private val requestLocation =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+            val isAllGranted = permissions.entries.all { it.value }
+            if (isAllGranted) {
+                println()
+            } else {
+                println()
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        requestPermission()
+    }
+
+    private fun requestPermission() {
+        if (isNotValidLocationPermission()) {
+            requestLocation.launch(
+                arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                )
+            )
+        }
+    }
+
+    private fun isNotValidLocationPermission(): Boolean {
+        val fineLocationPermission = ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        )
+        val coarseLocationPermission = ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        )
+        return fineLocationPermission != PackageManager.PERMISSION_GRANTED &&
+                coarseLocationPermission != PackageManager.PERMISSION_GRANTED
     }
 }
